411bd1bee81b495133b67fdea8ec7889088289a5
pnburanapur-png <pnburanapur@gmail.com>
2026-03-28 15:47:39 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 public class sample{
     public static void main(String[] args){
         System.out.println("hello world!");
@@ -10,4 +10,4 @@ public class sample{
         System.out.println("hello world!");
     }
 }
->>>>>>> 286a3e7b33607c5df5694812b72b89587d8ce29b
+
